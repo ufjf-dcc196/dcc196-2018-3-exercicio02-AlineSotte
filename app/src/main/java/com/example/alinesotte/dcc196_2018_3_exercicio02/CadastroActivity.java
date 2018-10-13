51aa1b2d3ae9598cfb92c4638cc371a5b4e1aaa8
Aline Sotte <alinedpaso@gmail.com>
2018-10-13 15:44:01 -0300

    Evento do botão cadatro listagem indo para cadastro

--- app/src/main/java/com/example/alinesotte/dcc196_2018_3_exercicio02/CadastroActivity.java
+++ app/src/main/java/com/example/alinesotte/dcc196_2018_3_exercicio02/CadastroActivity.java
@@ -1,15 +1,30 @@
 package com.example.alinesotte.dcc196_2018_3_exercicio02;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.List;
 
 public class CadastroActivity extends AppCompatActivity {
 
-
+  private Button cadastrook;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        cadastrook=(Button)findViewById(R.id.btncadastro);
+
+        cadastrook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent4= new Intent(CadastroActivity.this, ListagemActivity.class);
+                startActivity(intent4);
+            }
+        });
     }
 }
