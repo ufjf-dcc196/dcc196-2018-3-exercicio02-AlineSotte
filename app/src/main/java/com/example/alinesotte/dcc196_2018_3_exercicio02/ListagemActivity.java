3a939de8c8da38d23615f61057eab923f40110ad
Aline Sotte <alinedpaso@gmail.com>
2018-10-13 15:39:46 -0300

    Evento do botão cadastar na listagem

--- app/src/main/java/com/example/alinesotte/dcc196_2018_3_exercicio02/ListagemActivity.java
+++ app/src/main/java/com/example/alinesotte/dcc196_2018_3_exercicio02/ListagemActivity.java
@@ -1,7 +1,9 @@
 package com.example.alinesotte.dcc196_2018_3_exercicio02;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class ListagemActivity extends AppCompatActivity {
@@ -16,5 +18,13 @@ public class ListagemActivity extends AppCompatActivity {
 
         ircadastrar=(Button)findViewById(R.id.btncastrarlist);
         excluir=(Button)findViewById(R.id.btnexcluir);
+
+        ircadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent3 = new Intent(ListagemActivity.this,CadastroActivity.class);
+                startActivity(intent3);
+            }
+        });
     }
 }
